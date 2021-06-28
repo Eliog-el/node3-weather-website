70dca461c0d431c36cb2790b78a7782082e72872
Elijah Ogunrinade <elijah.ogunrinade@theagromall.com.ng>
2021-06-28 15:41:30 +0100

    set up app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
@@ -20,5 +20,5 @@ weatherForm.addEventListener('submit', (e) => {
                 messageTwo.textContent = data.forecast
             }
         })
-    }) 
+    })
 })
